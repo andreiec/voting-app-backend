3673370f017b82479a57c780a3cb19087ea96c8c
Andrei Constantinescu <andreiec112@gmail.com>
2022-06-10 20:12:08 +0300

    Patched email lost password

--- votingapp/src/signals.py
+++ votingapp/src/signals.py
@@ -49,6 +49,6 @@ def passwordResetTokenCreated(sender, instance, reset_password_token, *args, **k
     subject = "Resetare parolă - Platformă vot"
     message = f"Salut,\nAi primit acest mail deoarece ai cerut resetarea parolei. Pentru a continua, accesează link-ul de mai jos\n\nhttp://localhost:3000/reset-password/?token={reset_password_token.key}\n\nÎn cazul în care nu ai cerut acest mail te rugăm sa contactezi administratorul."
 
-    EmailThread(subject, message, settings.EMAIL_HOST_USER, [instance.email]).start()
+    EmailThread(subject, message, settings.EMAIL_HOST_USER, [reset_password_token.user.email]).start()
 
 post_save.connect(createUser, sender=User)
